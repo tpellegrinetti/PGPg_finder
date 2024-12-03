380c4cb42a78746324034a1e6ff9ce2c49f1f19a
Thierry Pellegrinetti <54033509+tpellegrinetti@users.noreply.github.com>
2024-12-03 10:55:10 -0300

    Update metafast_wf.sh

--- workflows/metafast_wf.sh
+++ workflows/metafast_wf.sh
@@ -2,18 +2,26 @@
 
 # Function to display help
 display_help() {
-    echo "Usage: $0 -i <input_directory> -o <output_directory> -t <number_of_threads> [-m <mode>]"
+    echo "PGPg_finder v1.1.0 | by Thierry Pellegrinetty <thierry.pellegrinetti@hotmail.com>"
+    echo "Check https://github.com/tpellegrinetti/PGPg_finder for updates"
+    echo "Usage: $0 -i <input_directory> -o <output_directory> -t <threads> [--piden <min_identity>] [--qcov <min_query_cover>] [--extra <extra_args>] [--bitscore <min_score>] [--evalue <evalue>] [--mode <mode>] [-h]"
     echo
-    echo "This script performs gene search with DIAMOND on a collection of metagenomes,"
-    echo "after quality trimming with Trimmomatic, merging paired reads with PEAR, and gene prediction with Prodigal."
+    echo "Read-Based alignment against PLaBAse on a collection of metagenomes,"
     echo "Results are saved to the output directory."
     echo
     echo "Arguments:"
-    echo "  -i   Path to the directory containing read files (_1.fq and _2.fq)."
-    echo "  -o   Path to the directory where results will be saved."
-    echo "  -t   Number of threads to be used by the tools."
-    echo "  -m   DIAMOND mode (optional)."
+    echo "  -i        Path to the directory containing genome files (.fasta, .fna, .fa)."
+    echo "  -o        Path to the directory where results will be saved."
+    echo "  -t        Number of CPU threads to be used in the pipeline."
     echo
+    echo "Optional Arguments:"
+    echo "  --piden     Minimum identity for DIAMOND (default: 30)."
+    echo "  --qcov      Minimum query coverage for DIAMOND (default: 50)."
+    echo "  --extra     Additional DIAMOND arguments (optional)."
+    echo "  --bitscore  Minimum bit score to report alignments."
+    echo "  --evalue    Maximum e-value to report alignments (default: 1e-5)."
+    echo "  --dmode     DIAMOND mode for sequence search (e.g., fast, sensitive, very-sensitive)."
+    echo "  -h          Display this help message."
 }
 
 # Function for logging
@@ -22,38 +30,60 @@ log() {
     echo "[${timestamp}] $1" | tee -a "$log_file"
 }
 
-# Check if no argument was provided
-if [ $# -eq 0 ]; then
+# Parse long and short options using `getopt`
+ARGS=$(getopt -o i:o:t:h --long piden:,qcov:,extra:,bitscore:,evalue:,dmode:,help -n "$0" -- "$@")
+if [ $? -ne 0 ]; then
+    display_help
+    exit 1
+fi
+eval set -- "$ARGS"
+
+# Default values
+min_identity=30
+min_query_cover=50
+diamond_extra=""
+min_score=""
+evalue="1e-5"
+diamond_mode=""
+
+# Parse options
+while true; do
+    case "$1" in
+        -i) genomes_dir=$2; shift 2 ;;
+        -o) out_dir=$2; shift 2 ;;
+        -t) threads=$2; shift 2 ;;
+        --piden) min_identity=$2; shift 2 ;;
+        --qcov) min_query_cover=$2; shift 2 ;;
+        --extra) diamond_extra=$2; shift 2 ;;
+        --bitscore) min_score=$2; shift 2 ;;
+        --evalue) evalue=$2; shift 2 ;;
+        --dmode) diamond_mode=$2; shift 2 ;;
+        -h|--help) display_help; exit 0 ;;
+        --) shift; break ;;
+        *) display_help; exit 1 ;;
+    esac
+done
+
+# Create log file
+log_file="${out_dir}/log.txt"
+touch "$log_file"
+
+# Validation of required arguments
+if [ -z "$genomes_dir" ] || [ -z "$out_dir" ] || [ -z "$threads" ]; then
+    log "Error: Missing required arguments."
     display_help
     exit 1
 fi
 
-# Parse command-line arguments
-diamond_mode=""
-while getopts i:o:t:m:h opt; do
-  case $opt in
-    i) reads_dir=$OPTARG ;;
-    o) out_dir=$OPTARG ;;
-    t) threads=$OPTARG ;;
-    m) diamond_mode=$OPTARG ;;
-    h) display_help
-       exit 0 ;;
-    *) display_help
-       exit 1 ;;
-  esac
-done
-
-# Get the directory path where the script is running
+# Check if DIAMOND database exists
 script_dir=$(dirname "$(dirname "$(readlink -f "$0")")")
-
-# Path to the DIAMOND database
 diamond_db="$script_dir/database/metagenome.dmnd"
 
-# Verify if required arguments were provided
-if [ -z "$reads_dir" ] || [ -z "$out_dir" ] || [ -z "$threads" ]; then
-    log "Missing arguments. See usage below:"
-    display_help
+if [ ! -f "$diamond_db" ]; then
+    log "Error: DIAMOND database not found at $diamond_db."
     exit 1
+else
+    log "DIAMOND database found at $diamond_db"
 fi
 
 # Create output directory if it doesn't exist
@@ -62,23 +92,19 @@ if [ ! -d "$out_dir" ]; then
     log "Created output directory: $out_dir"
 fi
 
-# Create log file
-log_file="${out_dir}/log.txt"
-touch "$log_file"
-
 # Create a file to store gene counts
 gene_counts_file="${out_dir}/gene_counts.txt"
 echo -e "Sample\tID\tCount" > "$gene_counts_file"
 log "Created gene counts file: $gene_counts_file"
 
 # Loop to iterate over each pair of read files
-for reads_1 in $reads_dir/*_1.*; do
+for reads_1 in "$genomes_dir"/*_*1.*; do
     sample=$(basename "$reads_1")
     sample=${sample%_1.*}
     log "Processing sample $sample..."
     
-    read_file_1=$reads_1
-    read_file_2=${reads_1/_1./_2.}
+    read_file_1="$reads_1"
+    read_file_2="${reads_1/_1./_2.}"
     trimmed_file_1="${out_dir}/${sample}_trimmed_1.fq"
     trimmed_file_2="${out_dir}/${sample}_trimmed_2.fq"
     trimmed_se="${out_dir}/${sample}_trimmed_se.fq"
@@ -87,55 +113,43 @@ for reads_1 in $reads_dir/*_1.*; do
     log "Running Trimmomatic for quality trimming"
     if [ -f "$read_file_2" ]; then
         # Paired-end reads
-        trimmomatic PE -threads $threads $read_file_1 $read_file_2 $trimmed_file_1 $trimmed_se $trimmed_file_2 $trimmed_se SLIDINGWINDOW:4:20 MINLEN:36
+        trimmomatic PE -threads "$threads" "$read_file_1" "$read_file_2" "$trimmed_file_1" "$trimmed_se" "$trimmed_file_2" "$trimmed_se" SLIDINGWINDOW:4:20 MINLEN:50
     else
         # Single-end reads
-        trimmomatic SE -threads $threads $read_file_1 $trimmed_file_1 SLIDINGWINDOW:4:20 MINLEN:36
+        trimmomatic SE -threads "$threads" "$read_file_1" "$trimmed_file_1" SLIDINGWINDOW:4:20 MINLEN:50
     fi
 
-    # Check if diamond_mode is set and add it to the DIAMOND command
-    if [ ! -z "$diamond_mode" ]; then
-        diamond_mode_flag="--mode $diamond_mode"
+    # Run DIAMOND
+    log "Running DIAMOND for PLaBAse alignment for ${sample}"
+    diamond blastx -d "$diamond_db" \
+        -q "$trimmed_file_1" \
+        -o "${out_dir}/${sample}_diamond.txt" \
+        -k 1 \
+        -p "$threads" \
+        -e "$evalue" \
+        --id "$min_identity" \
+        --query-cover "$min_query_cover" \
+        $( [ -n "$min_score" ] && echo "--min-score $min_score" ) \
+        $diamond_mode \
+        $diamond_extra
+
+    if [ $? -ne 0 ]; then
+        log "Error: DIAMOND failed for ${sample}."
+        exit 1
     else
-        diamond_mode_flag=""
+        log "Completed DIAMOND search for sample $sample"
     fi
 
-    if [ -f "$read_file_2" ]; then
-        # Paired-end reads
-        # Merge paired reads with PEAR
-        pear -f $trimmed_file_1 -r $trimmed_file_2 -o ${out_dir}/${sample}_merged -j $threads -q 20
-        log "Merged paired reads for sample $sample"
-    
-        # Run DIAMOND
-        diamond blastx -d $diamond_db -q ${out_dir}/${sample}_merged.assembled.fastq -o ${out_dir}/${sample}_diamond.txt -k 1 -e 0.0001 -p $threads $diamond_mode_flag
-        log "Completed DIAMOND search for sample $sample"
-
-        # Parse DIAMOND output and get gene counts
-        awk '{print $2}' ${out_dir}/${sample}_diamond.txt | sort | uniq -c | while read count id; do
-            echo -e "${sample}\t${id}\t${count}" >> $gene_counts_file
-        done
-        log "Generated gene counts for sample $sample"
-
-        # Clean up
-        rm -f ${out_dir}/${sample}_merged*
-    else
-        # Single-end reads
-        # Run DIAMOND directly on the single-end reads
-        diamond blastx -d $diamond_db -q $trimmed_file_1 -o ${out_dir}/${sample}_diamond.txt -k 1 -e 0.0001 -p $threads $diamond_mode_flag
-        log "Completed DIAMOND search for sample $sample"
-
-        # Parse DIAMOND output and get gene counts
-        awk '{print $2}' ${out_dir}/${sample}_diamond.txt | sort | uniq -c | while read count id; do
-            echo -e "${sample}\t${id}\t${count}" >> $gene_counts_file
-        done
-        log "Generated gene counts for sample $sample"
-    fi
+    # Parse DIAMOND output and get gene counts
+    awk '{print $2}' "${out_dir}/${sample}_diamond.txt" | sort | uniq -c | while read count id; do
+        echo -e "${sample}\t${id}\t${count}" >> "$gene_counts_file"
+    done
+    log "Generated gene counts for sample $sample"
 done
 
 log "Gene search is completed. Check ${gene_counts_file} for the results."
 
 # Python script to generate the heatmaps
 heatmap_script="$script_dir/vis-scripts/heatmap_plabase.py"
-python "$heatmap_script" "${gene_counts_file}" "${out_dir}" "$script_dir/database/pathways_plabase.txt" "$script_dir/database/summary.txt"
+python "$heatmap_script" "$gene_counts_file" "$out_dir" "$script_dir/database/pathways_plabase.txt" "$script_dir/database/summary.txt"
 log "Generated heatmaps"
-
